0094fe57c0e821cb4d8430c22a38e546f6104c8c
Lucas Oliveira <lucas_7tv@hotmail.com>
2025-08-12 07:48:39 -0300

    Feat: Add get_user_by_phone_number method to UserService class

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -8,3 +8,7 @@ class UserService:
     @staticmethod
     def get_user_by_email(user_email: str):
         return UserRepository.get_user_by_email(user_email)
+
+    @staticmethod
+    def get_user_by_phone_number(user_phone_number: str):
+        return UserRepository.get_user_by_phone_number(user_phone_number)
