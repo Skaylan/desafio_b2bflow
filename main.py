7d57b9f078df30a06ed58bb2caf28ac4e477efb2
Lucas Oliveira <lucas_7tv@hotmail.com>
2025-08-12 08:29:38 -0300

    Feat: Implement main function to send greeting message to all users

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+from app.controllers.message_controller import MessageController
+
+def main():
+    menssagem = "Olá {name}, tudo bem com você?"
+    MessageController.send_message_to_all_users(menssagem)
+
+if __name__ == "__main__":
+    main()
