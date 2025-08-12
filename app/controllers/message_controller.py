fde7d06dfc0c4d425f64af4f0cee3e61e6409fd3
Lucas Oliveira <lucas_7tv@hotmail.com>
2025-08-12 08:19:11 -0300

    Feat: Add send_message_by_user_email method to MessageController class

--- app/controllers/message_controller.py
+++ app/controllers/message_controller.py
@@ -12,3 +12,12 @@ class MessageController:
                 MessageService.send_message(personalized_message , user["phone_number"])
         except Exception as e:
             print(e)
+
+    @staticmethod
+    def send_message_by_user_email(user_email: str, message: str):
+        try:
+            user = UserService.get_user_by_email(user_email)
+            message = message.format(name=user["name"])
+            MessageService.send_message(message, user["phone_number"])
+        except Exception as e:
+            print(e)
